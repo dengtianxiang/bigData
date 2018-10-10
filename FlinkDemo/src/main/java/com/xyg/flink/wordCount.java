a66da2f60e0742cc78915917c5a1ade11930bfe5
Frankdeng <409167848@qq.com>
2018-10-10 11:55:58 +0800

    Big data learning

---
+++ FlinkDemo/src/main/java/com/xyg/flink/wordCount.java
@@ -0,0 +1,40 @@
+package com.xyg.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+/**
+ * Author: Mr.Deng
+ * Date: 2018/10/9
+ * Desc:
+ */
+public class wordCount {
+    public static void main(String[] args) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<String> text = env.fromElements("I love bigData hadoop spark flink flink");
+        DataSet<Tuple2<String, Integer>> wordCounts =
+                // split up the lines in pairs (2-tuples) containing: (word,1)
+                text.flatMap(new Tokenizer())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .groupBy(0)
+                        .sum(1);
+        wordCounts.print();
+    }
+
+    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split(" ");
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
+        }
+    }
+}
